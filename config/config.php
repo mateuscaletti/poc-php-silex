d386c95b88f411882d54041b639246a242959b60
Mateus Caletti <mateus@caletti.com.br>
2018-05-16 20:53:48 -0300

    Ajustando variáveis no config.php

--- config/config.php
+++ config/config.php
@@ -1,24 +1,25 @@
 <?php
 
 //ini_set('display_errors', 0);
-ini_set('display_errors', 1);
+ini_set('display_errors', getenv('APP_DISPLAYERRORS'));
 
-//error_reporting(E_ERROR | E_WARNING | E_PARSE);
 error_reporting(E_ALL);
+if(getenv('APP_ERROR_LEVEL') != 'E_ALL')
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 $SCFconfig = array();
 
 $SCFconfig['debug'] = true;
 
 $SCFconfig['db.driver'] = 'pdo_mysql';
-$SCFconfig['db.dbhost'] = 'localhost';
-$SCFconfig['db.host'] = 'localhost';
-$SCFconfig['db.dbname'] = '';
-$SCFconfig['db.user'] = '';
-$SCFconfig['db.password'] = '';
+$SCFconfig['db.dbhost'] = getenv('APP_DB_HOST');
+$SCFconfig['db.host'] = getenv('APP_DB_HOST');
+$SCFconfig['db.dbname'] = getenv('APP_DB_DBNAME');
+$SCFconfig['db.user'] = getenv('APP_DB_USER');
+$SCFconfig['db.password'] = getenv('APP_DB_PASS');
 $SCFconfig['db.charset'] = 'utf8';
 
-$SCFconfig['login.salt'] = ' asd 2382y3rbkj as JJ//? **34 9)(*"KÇÇÇk 12+';
+$SCFconfig['login.salt'] = getenv('APP_LOGIN_SALT');
 
-$SCFconfig['logs.profiler.enabled'] = false;
-$SCFconfig['logs.monolog.enabled'] = true;
+$SCFconfig['logs.profiler.enabled'] = getenv('APP_LOGS_PROFILER_ENABLED');
+$SCFconfig['logs.monolog.enabled'] = getenv('APP_LOGS_MONOLOG_ENABLED');
